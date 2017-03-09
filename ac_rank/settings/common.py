e4e99dcd826df7abd379bdb275a7daec1175be24
ev-agelos <evagelos.theo@gmail.com>
2017-03-09 21:59:03 +0100

    add static dir path

--- ac_rank/settings/common.py
+++ ac_rank/settings/common.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 INTERNAL_IPS = ['127.0.0.1']
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
