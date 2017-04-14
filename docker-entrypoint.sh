ca9f27b1f1c65ae47d1c74778f45362ac8410b7f
ev-agelos <evagelos.theo@gmail.com>
2017-04-14 11:01:04 +0200

    add automatic superuser creation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,24 @@ python manage.py collectstatic --noinput
 echo "Apply database migrations"
 python manage.py migrate
 
+# Create superuser if does not exist
+echo "Create superuser if does not exist"
+script="
+from django.contrib.auth.models import User;
+
+username = '$SUPERUSER';
+password = '$SUPERUSER_PASS';
+email = '$SUPERUSER_MAIL';
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
+
 # Start server
 echo "Starting server"
+echo "Superuser is $SUPERUSER"
 gunicorn ac_rank.wsgi:application --bind :9001 --workers=2
