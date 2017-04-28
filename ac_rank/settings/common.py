4488a87ad082026cca68a99675573dfe45321c52
ev-agelos <evagelos.theo@gmail.com>
2017-04-28 20:38:18 +0200

    hook up sentry

--- ac_rank/settings/common.py
+++ ac_rank/settings/common.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'raven.contrib.django.raven_compat',
     'tokenapi',
     'anymail',
     'laptimes',
