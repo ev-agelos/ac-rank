967d375a9be594c576a5ec7d59145b234736fa4e
ev-agelos <evagelos.theo@gmail.com>
2019-01-01 12:59:06 +0100

    set dev settings in manage.py

--- ac_rank/settings/dev.py
+++ ac_rank/settings/dev.py
@@ -9,3 +9,13 @@ MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'localhost',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
+    }
+}
